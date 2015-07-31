bcaa72344069afe09d0df511acd92fb844a885f8
OkramL <livental@gmail.com>
2015-07-31 15:13:06 +0300

    removed unused import

--- src/gpstokml4/ReadingFolder.java
+++ src/gpstokml4/ReadingFolder.java
@@ -6,7 +6,6 @@
 package gpstokml4;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
@@ -17,7 +16,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.regex.Pattern;
 
 /**
  *
